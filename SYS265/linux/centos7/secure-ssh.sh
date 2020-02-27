57af596439f58f65957408037b269bf52fa9b22d
jacktbroph <jack.brophy@mymail.champlain.edu>
2020-02-27 18:12:43 +0000

    added pub key

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -2,14 +2,14 @@
 
 NewUser="$1"
 
-useradd -m -d /home/${NewUser} -s /bin/bash ${NewUser}
+sudo useradd -m -d /home/${NewUser} -s /bin/bash ${NewUser}
 
-mkdir /home/${NewUser}/.ssh
+sudo mkdir /home/${NewUser}/.ssh
 
 # Adds SSH Public Key from local repo
 
-cp SYS265/linux/public-keys/id_rsa.pub /home/${NewUser}/.ssh/authorized_keys
-chmod 700 /home/${NewUser}/.ssh
-chmod 600 /home/${NewUser}/.ssh/authorized_keys
-chown -R ${NewUser}:${NewUser} /home/${NewUser}/.ssh
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${NewUser}/.ssh/authorized_keys
+sudo chmod 700 /home/${NewUser}/.ssh
+sudo chmod 600 /home/${NewUser}/.ssh/authorized_keys
+sudo chown -R ${NewUser}:${NewUser} /home/${NewUser}/.ssh
 echo ${NewUser}
